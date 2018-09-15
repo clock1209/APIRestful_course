a721be308850bddbdb023bcf0b63afcbb68bda8c
Octavio Cornejo <saber.oct@live.com>
2018-09-15 16:06:20 -0500

    Commit inicial.

---
+++ app/User.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    const USER_VERIFIED = '1';
+    const USER_NOT_VERIFIED = '0';
+
+    const USER_ADMINISTRATOR = 'true';
+    const USER_REGULAR = 'false';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'verified',
+        'verification_token',
+        'admin',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'verification_token',
+    ];
+
+    public function isVerified()
+    {
+        return $this->verified == static::USER_VERIFIED;
+    }
+
+    public function isAdministrator()
+    {
+        return $this->admin = static::USER_ADMINISTRATOR;
+    }
+
+    public function generateVerificationToken()
+    {
+        return str_random(40);
+    }
+}
