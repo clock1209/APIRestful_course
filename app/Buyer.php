836ac8bd14a3201563d4c695022d7a62a4690831
Octavio Cornejo <saber.oct@live.com>
2018-09-15 23:46:12 -0500

    Sección 12 | operaciones en BuyerController.

--- app/Buyer.php
+++ app/Buyer.php
@@ -4,7 +4,7 @@ namespace App;
 
 class Buyer extends User
 {
-    public function transaction()
+    public function transactions()
     {
         return $this->hasMany(Transaction::class);
     }
--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Controllers\Buyer;
 
-use Illuminate\Http\Request;
+use App\Buyer;
 use App\Http\Controllers\Controller;
 
 class BuyerController extends Controller
 {
-    //
+    /**
+     * @author Octavio Cornejo <octavio.cornejo@nuvemtecnologia.mx>
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $buyers = Buyer::has('transactions')->get();
+
+        return response()->json(['data' => $buyers], 200);
+    }
+
+    /**
+     * @author Octavio Cornejo <octavio.cornejo@nuvemtecnologia.mx>
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        $buyer = Buyer::has('transactions')->findOrFail($id);
+
+        return response()->json(['data' => $buyer], 200);
+    }
 }
