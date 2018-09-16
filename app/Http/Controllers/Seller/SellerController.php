38884a29f798a4c63ddf6a8d5be76f7188251402
Octavio Cornejo <saber.oct@live.com>
2018-09-15 23:51:34 -0500

    Sección 13 | Operaciones de SellerController.

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers\Seller;
 
+use App\Seller;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class SellerController extends Controller
 {
-    //
+    /**
+     * @author Octavio Cornejo <octavio.cornejo@nuvemtecnologia.mx>
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $sellers = Seller::has('products')->get();
+
+        return response()->json(['data' => $sellers], 200);
+    }
+
+    /**
+     * @author Octavio Cornejo <octavio.cornejo@nuvemtecnologia.mx>
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        $seller = Seller::has('products')->findOrFail($id);
+
+        return response()->json(['data' => $seller], 200);
+    }
 }
