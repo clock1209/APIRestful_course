46a68638c2ba8c63d18ab056a5ccb7d2d3c859cb
Octavio Cornejo <saber.oct@live.com>
2018-09-22 12:02:12 -0500

    Sección 18 | Implementando las operaciones para category.

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers\Category;
 
+use App\Category;
+use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
 
 class CategoryController extends ApiController
 {
-    //
+    public function index()
+    {
+        $categories = Category::all();
+        return $this->showAll($categories);
+    }
+
+    public function show(Category $category)
+    {
+        return $this->showOne($category);
+    }
+
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+        ];
+
+        $this->validate($request, $rules);
+
+        $category = Category::create($request->all());
+
+        return $this->showOne($category, 201);
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        $category->fill($request->only([
+            'name',
+            'description',
+        ]));
+
+        if ($category->isClean()) {
+            return $this->errorResponse('Debe especificar al menos un valor diferente para actualizar.', 422);
+        }
+
+        return $this->showOne($category);
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return $this->showOne($category);
+    }
 }
