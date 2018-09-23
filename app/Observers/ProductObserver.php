4e0615d0525f83a45cb98775c3dcdfb47c01f55f
Octavio Cornejo <saber.oct@live.com>
2018-09-23 01:43:35 -0500

    Sección 25 | Implementando las operaciones complejas para Product.

---
+++ app/Observers/ProductObserver.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Observers;
+
+use App\Product;
+
+class ProductObserver
+{
+    /**
+     * Handle the product "created" event.
+     *
+     * @param  \App\Product  $product
+     * @return void
+     */
+    public function created(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the product "updated" event.
+     *
+     * @param  \App\Product  $product
+     * @return void
+     */
+    public function updated(Product $product)
+    {
+        if ($product->quantity == 0 and $product->isAvailable()) {
+            $product->status = Product::PRODUCT_NOT_AVAILABLE;
+            $product->save();
+        }
+    }
+
+    /**
+     * Handle the product "deleted" event.
+     *
+     * @param  \App\Product  $product
+     * @return void
+     */
+    public function deleted(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the product "restored" event.
+     *
+     * @param  \App\Product  $product
+     * @return void
+     */
+    public function restored(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the product "force deleted" event.
+     *
+     * @param  \App\Product  $product
+     * @return void
+     */
+    public function forceDeleted(Product $product)
+    {
+        //
+    }
+}
