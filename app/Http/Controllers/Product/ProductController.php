7e38cf4cc812494f103b02855e3d46a4cecba910
Octavio Cornejo <saber.oct@live.com>
2018-09-22 19:21:08 -0500

    Sección 19 | Implementar las operaciones para Product.

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers\Product;
 
 use App\Http\Controllers\ApiController;
+use App\Product;
 
 class ProductController extends ApiController
 {
-    //
+    public function index()
+    {
+        $products = Product::all();
+
+        return $this->showAll($products);
+    }
+
+    public function show(Product $product)
+    {
+        return $this->showOne($product);
+    }
 }
